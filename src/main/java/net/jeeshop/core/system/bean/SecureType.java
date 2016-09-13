6becd0ffe5808c6ce88af849110e83998f346677
lin <464565830@qq.com>
2016-09-13 16:50:57 +0800

    Revert "lyx"
    This reverts commit b7e8702464b0deaaf08b600adbe52fb80366b1d8.

---
+++ src/main/java/net/jeeshop/core/system/bean/SecureType.java
@@ -0,0 +1,22 @@
+package net.jeeshop.core.system.bean;
+
+/**
+ * 资源类型
+ * @author huangf
+ *
+ */
+public enum SecureType {
+	/**
+	 * 模块
+	 */
+	module, 
+	/**
+	 * 页面
+	 */
+	page, 
+	
+	/**
+	 * 按钮、功能
+	 */
+	button
+}
