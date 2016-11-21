54783811f79a145f0de3008f3a012bd5c3b15138
changshengmen <1445972127@qq.com>
2016-11-21 11:23:03 +0800

    接口调试

--- src/main/java/net/jeeshop/core/util/webServiceUtil.java
+++ src/main/java/net/jeeshop/core/util/webServiceUtil.java
@@ -7,28 +7,30 @@ import org.apache.axis.client.Service;
 import org.apache.axis.encoding.XMLType;
 
 /**
- * @author lin
- * axis1.4 调用webservice接口
+ * @author lin axis1.4 调用webservice接口
  */
-public class webServiceUtil {
-	public static String getSupplyInfo(String json,String url){
+public class webServiceUtil
+{
+	public static String getSupplyInfo(String json, String url,String operationName)
+	{
 		String result = "";
-		try {
+		try
+		{
 			Service service = new Service();
 			Call call = (Call) service.createCall();
 			call.setTargetEndpointAddress(url);
-			call.setOperationName("NvhlTotal");
+			call.setOperationName(operationName);
 			call.addParameter("TotalParValue", XMLType.XSD_STRING, ParameterMode.IN);
 			call.setReturnType(XMLType.XSD_STRING);
-			 // 调用并且获取返回的报文结果
-			   result = (String) call.invoke(new Object[] { json });
-			   System.out.println("=================>" + result);
-			  } catch (Exception e)
-			  {
-			   System.out.println("GeneratorData_error_supply:" + e);
-			  }
-			  System.out.println("result="+result);
-			  return result;
+			// 调用并且获取返回的报文结果
+			result = (String) call.invoke(new Object[] { json });
+			System.out.println("=================>" + result);
+		} catch (Exception e)
+		{
+			System.out.println("GeneratorData_error_supply:" + e);
+		}
+		System.out.println("result=" + result);
+		return result;
 
 	}
 }
